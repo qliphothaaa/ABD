3fab702ecf0a623bdf8256fcc389b876095f3898
qliphoth <paradox.qliphoth@gmail.com>
2019-10-01 22:36:19 +0700

    ver 1.0

---
+++ test3.py
@@ -0,0 +1,16 @@
+import unittest
+from map_object import MapObject
+from map_object_type import ObjectType
+from map_scene import Scene
+
+class TestMapObject(unittest.TestCase):
+    def setUp(self):
+        self.scene = Scene('test')
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
